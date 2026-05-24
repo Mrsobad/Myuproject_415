ca6bb4a87c07b2639b647180a58cf569c3cf641f
Mrsobad <155410933+Mrsobad@users.noreply.github.com>
2026-05-23 22:36:06 -0700

    DMI

---
+++ Source/MyProject415/CueDMIMod.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CueDMIMod.h"
+
+// Sets default values
+ACueDMIMod::ACueDMIMod()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACueDMIMod::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACueDMIMod::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
